520c84d332150af2a37b39ebef899ff2354e6a31
Guilherme Mendes <guihenrique2806@gmail.com>
2022-03-08 11:25:16 -0300

    att

--- fpoo/workexoo2/exoo2/src/ex/Main.java
+++ fpoo/workexoo2/exoo2/src/ex/Main.java
@@ -8,52 +8,56 @@ public class Main {
 	static Scanner s = new Scanner(System.in);
 	public static void main(String[] args) {
 		
-		int qtde;
 		
-		do {
-			
-			System.out.println("Digite quantos alunos deseja cadastrar (max. 10):");
-			qtde = s.nextInt();
-			
-			if(qtde > 10) {
-				System.out.println("Valor excede o limite");
-			}
-			
-		} while(qtde > 10);
+		Aluno[] alunos = new Aluno[10];
 		
-		
-		Aluno[] alunos = new Aluno[qtde];
-		
-		for (int i = 0; i < qtde; i++) {
-			System.out.println("Digite o nome do Aluno");
-			String nome = s.next();
-			
-			System.out.println("Digite a primeira nota");
-			double nota1 = s.nextDouble();
-			
-			System.out.println("Digite a segunda nota");
-			double nota2 = s.nextDouble();
-			
-			System.out.println("Digite a terceira nota");
-			double nota3 = s.nextDouble();
-			
-			int matricula = i+1;
-			
-			alunos[i] = new Aluno(nome, matricula, nota1, nota2, nota3);
-			
-			System.out.println("__________________________");
+		for (int i = 0; i < alunos.length; i++) {
+			alunos[i] = null;
 		}
 		
 		System.out.println("Digite uma opção");
 		
 		int menu = 0;
 		
-		while(menu != 3) {
-			System.out.println("1 - Buscar por número de matrícula\n2 - Buscar por nome\n3 - Sair");
+		while(menu != 4) {
+			System.out.println("1 - Cadastrar novo Usuário\n2 - Buscar por número de matrícula\n3 - Buscar por nome\n4 - Sair");
 			menu = s.nextInt();
 			
 			switch (menu) {
-			case 1: 
+			
+			case 1:
+				
+				for (int i = 0; i < alunos.length; i++) {
+					if (alunos[9] == null) {
+						if(alunos[i] == null) {
+							System.out.println("Digite o nome do Aluno");
+							String nome = s.next();
+							
+							System.out.println("Digite a primeira nota");
+							double nota1 = s.nextDouble();
+							
+							System.out.println("Digite a segunda nota");
+							double nota2 = s.nextDouble();
+							
+							System.out.println("Digite a terceira nota");
+							double nota3 = s.nextDouble();
+							
+							int matricula = i+1;
+							
+							alunos[i] = new Aluno(nome, matricula, nota1, nota2, nota3);
+							
+							System.out.println("__________________________");
+							break;
+						}
+					} else {
+						System.out.println("Limite Excedido");
+						break;
+					}
+				}
+				
+				break;
+			
+			case 2: 
 				
 				System.out.println("\nDigite o número de matrícula para ser buscado:");
 				int n = s.nextInt();
@@ -61,22 +65,7 @@ public class Main {
 				System.out.println("\nMatrícula\tNome\tNota 1\tNota 2\tNota 3");
 				
 				for (int i = 0; i < alunos.length; i++) {
-					if(alunos[i].matricula == n) {
-						alunos[i].mostraAluno();
-					}
-				}
-				
-				break;
-				
-			case 2:
-				
-				System.out.println("Digite o nome para ser buscado:");
-				String nome = s.next();
-				
-				System.out.println("\nMatrícula\tNome\tNota 1\tNota 2\tNota 3");
-				
-				for (int i = 0; i < alunos.length; i++) {
-					if(alunos[i].nome.equals(nome)) {
+					if(alunos[i] != null && alunos[i].matricula == n) {
 						alunos[i].mostraAluno();
 					}
 				}
@@ -84,6 +73,21 @@ public class Main {
 				break;
 				
 			case 3:
+				
+				System.out.println("Digite o nome para ser buscado:");
+				String nome = s.next();
+				
+				System.out.println("\nMatrícula\tNome\tNota 1\tNota 2\tNota 3");
+				
+				for (int i = 0; i < alunos.length; i++) {
+					if(alunos[i] != null && alunos[i].nome.equals(nome)) {
+						alunos[i].mostraAluno();
+					}
+				}
+				
+				break;
+				
+			case 4:
 				
 				System.out.println("Adeus");
 			
